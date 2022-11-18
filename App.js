7b16e7825ac84fa14859134ed3ee61665694f1a7
woong <deu04287@naver.com>
2022-11-18 10:09:35 +0900

    async_storage_study

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 // import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState } from 'react';
 import { ScrollView, StatusBar,SafeAreaView, StyleSheet, Text, View ,Platform  } from 'react-native';
 import { theme } from './colors';
@@ -7,18 +8,49 @@ import Footer from './Footer';
 import First from './Pages/First';
 
 
+const HAS_LAUNCHED = 'hasLaunched';
+
+function setAppLaunched() {
+  AsyncStorage.setItem(HAS_LAUNCHED, 'truely');
+}
+const storeData = async (value) => {
+  try {
+    await AsyncStorage.setItem('HAS_LAUNCHED', 'true')
+  } catch (e) {
+    // saving error
+  }
+}
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('HAS_LAUNCHED')
+    if(value !== null) {
+      // value previously stored
+    }
+  } catch(e) {
+    // error reading value
+  }
+}
 
 export default function App() {
-
-  
   const [first, setFirst] = useState(true); //처음 설치시
+  // AsyncStorage.setItem('HAS_LAUNCHED', 'sdfsasdf');
+  storeData();
+  // AsyncStorage.clear();
+  // console.log("234");
+  // console.log(setAppLaunched());
+  // console.log(getData());
+  // AsyncStorage.setItem('nickname','User2');
+
+  AsyncStorage.getItem('HAS_LAUNCHED', (err, result) => {
+    console.log(result); // User1 출력
+  });
+
 
   if(first === true){
     return (
         <First/>
     );
   }
-
   else if(first === false){
     return(
       <View style={{flex: 1}}>
--- Pages/First.js
+++ Pages/First.js
@@ -6,7 +6,7 @@ export default function First() {
 
       return (
         <View>
-            <Text>falsesdfsdf</Text>
+            <Text>falsesdfssdfsdfsdfsdfdf</Text>
         </View>
       );
   }
